708f27f7cc195359a03af8c49419c95daab7d462
Jnani Weibel <jnaneshwar1234@gmail.com>
2016-11-19 14:25:04 -0800

    Build coaches and managers view

--- routes/coachAPI.js
+++ routes/coachAPI.js
@@ -1,31 +1,29 @@
 var express = require('express');
 var db = require('../db');
 var router = express.Router();
-var helpers = require('./helperFunctions');
 
-router.get('/', function(req,res,next){
-  db.query('SELECT * FROM coaches WHERE deleted = 0;', 
+router.get('/', function(req, res, next) {
+  var query = 'SELECT * FROM coaches WHERE deleted != 1;';
+
+  db.query(query, 
     function(err){
-      res.render('error', {message: err.message, error: err});
+      res.render('error', { message: err.message, error: err });
     },
     function(result){
-      res.render('results', {results:result});
+      res.render('results', { results:result });
     });
 });
 
-router.get('/:attributes/', function(req,res,next){
-  helpers.querySelectAndRoute('coaches', req,res,next);
-});  
+router.get('/include-deleted', function(req, res, next) {
+  var query = 'SELECT * FROM coaches;';
 
-
-router.get('/:attributes/:conditions/:operators/:boundaries', function(req,res,next){
-  helpers.querySelectAndRoute('coaches', req,res,next);
-});  
-
-
-router.get('/:attributes/:conditions/:operators/:boundaries/:logic', function(req,res,next){
-  helpers.querySelectAndRoute('coaches', req, res, next);
-});  
-
+  db.query(query, 
+    function(err){
+      res.render('error', { message: err.message, error: err });
+    },
+    function(result){
+      res.render('results', { results:result });
+    });
+});
 
 module.exports = router;
