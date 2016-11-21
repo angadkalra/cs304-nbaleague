c66eaa2150d026b7d02660045c359bb541e609f4
Nima Samiei <nima2322@hotmail.com>
2016-11-21 11:59:12 -0800

    reorganizing authentication and routes

--- routes/home.js
+++ routes/home.js
@@ -1,37 +1,10 @@
 var express = require('express');
 var db = require('../db.js');
 var router = express.Router();
-var loggedIn = false;
 var permissionLevel = "";
 
-router.get('/admins', function(req, res, next) {
-  if(loggedIn){
-    if(permissionLevel!="general"){
-      res.render('admins');}
-    else{
-      res.redirect('/'+permissionLevel);
-    }
-  }else{
-    res.redirect('/');
-  }
-});
-
-router.get('/general', function(req, res, next) {
-  res.render('general');
-});
-
-router.get('/', function(req, res, next){
-  if(!loggedIn){res.render('home')}else{res.redirect('/' +permissionLevel)};
-});
-
-router.get('/logout', function(req,res,next){
-  loggedIn=false;
-  permissionLevel = "";
-  res.redirect('/');
-});
-
-router.get('/:role/login/:name', function(req,res,next){
-  query = 'SELECT name FROM ' + req.params['role'] +' WHERE name = \'' +req.params['name']+'\';';
+router.get('/adminPage/:role/:name', function(req, res, next) {
+   query = 'SELECT name FROM ' + req.params['role'] +' WHERE name = \'' +req.params['name']+'\';';
   console.log(query);
   db.query(query, function(error){
     res.render('error');
@@ -43,12 +16,28 @@ router.get('/:role/login/:name', function(req,res,next){
     }else if(results.length==0){
       res.redirect('/');
     }else{
-      loggedIn=true;
-      permissionLevel = req.params['role'];
-      res.redirect('/'+permissionLevel);
+       permissionLevel=req.params['role'];
+       if(permissionLevel!="general"){
+         res.render('admins', {level: permissionLevel});
+       }else{
+         res.redirect('/general');
+       }
     }
   });
 
+});
+
+router.get('/general', function(req, res, next) {
+  res.render('general');
+});
+
+router.get('/', function(req, res, next){
+  res.render('home');
+});
+
+router.get('/logout', function(req,res,next){
+  permissionLevel = "";
+  res.redirect('/');
 });
 
 module.exports = router;
