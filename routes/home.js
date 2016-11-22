04dae1af55e664ef2f702262cb992b59d90000bc
Nima Samiei <nima2322@hotmail.com>
2016-11-21 18:37:05 -0800

    changing view routing

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,11 @@ var db = require('../db.js');
 var router = express.Router();
 var permissionLevel = "";
 
-router.get('/adminPage/:role/:name', function(req, res, next) {
-   query = 'SELECT name FROM ' + req.params['role'] +' WHERE name = \'' +req.params['name']+'\';';
+router.get('/home/:role/:name', function(req, res, next) {
+  if(req.params["role"] == "general"){
+    return res.render('home', {level:"general"});
+  }
+  query = 'SELECT name FROM ' + req.params['role'] +' WHERE name = \'' +req.params['name']+'\';';
   console.log(query);
   db.query(query, function(error){
     res.render('error');
@@ -17,22 +20,14 @@ router.get('/adminPage/:role/:name', function(req, res, next) {
       res.redirect('/');
     }else{
        permissionLevel=req.params['role'];
-       if(permissionLevel!="general"){
-         res.render('admins', {level: permissionLevel});
-       }else{
-         res.redirect('/general');
-       }
+       res.render('home', {level: permissionLevel});
     }
   });
 
 });
 
-router.get('/general', function(req, res, next) {
-  res.render('general');
-});
-
 router.get('/', function(req, res, next){
-  res.render('home');
+  res.render('login');
 });
 
 router.get('/logout', function(req,res,next){
